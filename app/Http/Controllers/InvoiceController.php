0a9a3bcaa687d5c8c0367092113d3e60af16fe5f
penda digital <info@penda.digital>
2025-11-12 13:49:14 +0200

    Add Log import to InvoiceController

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -10,6 +10,7 @@ use App\Models\Quote;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Inertia\Inertia;
 
